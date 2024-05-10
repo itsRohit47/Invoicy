81f41448e15328b1057aa1ae10758e33b5b2d004
Rohit Bajaj <rohitbajaj13@yahoo.com>
2024-05-10 23:16:45 +1000

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import { AddCustomers } from '@/app/ui/customers/buttons';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers | Invoicy Dashboard',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search Customers..." />
+        <AddCustomers />
+      </div>
+      <Table query={query} currentPage={currentPage} />
+    </div>
+  );
 }
